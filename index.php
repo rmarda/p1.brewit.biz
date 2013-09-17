81f9fa6544c94d1afb5c2b38ad5ebd90f30a655a
Ruchika Marda <rmarda4@gmail.com>
2013-09-16 23:44:54 -0400

    modified page to add bio and code editor I'm using

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
-Welcome to P1!
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Project P1</title>
+        <style>
+            
+            body {
+              font-family: Verdana;
+              font-size: small;
+            }
+
+            div {
+              margin-top: 10px;
+              margin-bottom: 30px;
+              margin-left: 15px;
+              margin-right: 15px;
+            }
+
+            .bio  {
+              font-family: Trebuchet MS;
+              font-size: large;
+              background: #FFFF99;
+              padding: 30px;
+              border-radius: 20px;
+            }
+
+        </style>
+    </head>
+    <body>
+        
+        <div class="bio">
+            <p>
+                Hi, My name is Ruchika Marda. I live in Natick, MA. I am originally from Kolkata, India. 
+            </p>
+            <p>
+                I work as a software engineer at MathWorks, Inc, Natick, MA. Back in 2005, I had dabbled in writing JSP pages but other than that, I have no experience developing web applications. I hope to learn a lot from this class. 
+            </p>
+            <p>
+                I am experimenting with Sublime as my code editor and I like it so far. 
+            </p>
+        </div>
+    </body>
+</html>
